efb33e95c4fcb5d1e7bd7c30ef0511d4ab261c51
As <87636037+alazarlemma@users.noreply.github.com>
2021-10-06 21:57:02 -0500

    naming

---
+++ naming-server-Eureka/src/main/java/com/sa/estore/namingservereureka/NamingServerEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.sa.estore.namingservereureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamingServerEurekaApplication.class, args);
+    }
+
+}
